2ffa2f9630a68fb6256dd4a63cc5127f02738159
sumin93 <suminfx@mail.ru>
2022-11-24 15:29:17 +0300

    Add vk icon and DarkBlue color

---
+++ app/src/main/java/com/sumin/vknewsclient/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.sumin.vknewsclient.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Black500 = Color(0xff9a9b9d)
+val Black900 = Color(0xff191919)
+val DarkBlue = Color(0xff4c75a3)
